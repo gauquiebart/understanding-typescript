93ae66a27da6c1ed40d40fd4b132183de6d7c353
Bart GAUQUIE <bart.gauquie@sfpd.fgov.be>
2022-10-25 14:10:37 +0200

    remaining modules

--- dist/section04/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const userNamez = `Max`;
-//userNamez = `Maxi`; // does not work ... 
-let age = 30;
-age = 29;
-const adder = (a = 1, b) => a + b;
-console.log(adder(undefined, 33));
-//# sourceMappingURL=app.js.map
